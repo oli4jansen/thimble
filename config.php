b218933a7b35c8d90824204b9eca046ca1e9c677
Olivier Jansen <oli4jansen.nl@gmail.com>
2013-10-04 20:03:27 +0200

    Header button hover fix

--- config.php
+++ config.php
@@ -6,6 +6,6 @@ $LOCALE = 'en-us.yml';
 $THEME_LOCATION = 'themes/'; // With trailing slash
 $DATA_LOCATION = 'data/'; // With trailing slash
 
-$TUMBLR_API_PUBLIC_KEY = 'ngXMfOBor93Z7ikxny6lPG1MB1UBJMvZgVugLElSWsBaytvbrp'; // For importing
+$TUMBLR_API_PUBLIC_KEY = 'your key here'; // For importing blogs
 
 ?>
